91934555dd411ef193b9e551f796d7471443de8a
David Dominguez <ddominguez@gmail.com>
2016-06-29 13:50:51 -0400

    Added func expression exercise

---
+++ multiple-returns-func-expression/main.go
@@ -0,0 +1,26 @@
+/*
+Write a func expression which takes an integer. The func will have two returns.
+The first return should be the argument divided by 2.
+The second return should be a bool that let’s us know whether or not the argument was even.
+For example:
+half(1) returns (0, false)
+half(2) returns (1, true)
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	half := func(a int) (int, bool) {
+		quotient := a / 2
+		var isEven bool
+		if a%2 == 0 {
+			isEven = true
+		} else {
+			isEven = false
+		}
+		return quotient, isEven
+	}
+	fmt.Println(half(2))
+}
